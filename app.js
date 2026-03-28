d92a674d33b705d6571dd98ff0f73d7c141f7b6b
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2026-03-28 11:25:50 +0100

    Eseguito route grouping delle rotte crud definite in precedenza

--- app.js
+++ app.js
@@ -1,32 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-//index
-app.get('/posts', (req,res) => {
-    res.send('Lista dei posts')
-})
+const postsRouter = require('./routers/posts.js');
 
-//show
-app.get('/posts/:id', (req,res) => {
-    res.send('Viasualizzazione del post ' + req.params.id)
-})
-
-//store
-app.post('/posts', (req,res) => {
-    res.send('Creazione nuovo post')
-})
-
-//update
-app.put('/posts/:id', (req,res) => {
-    res.send('Modifica del post ' + req.params.id)
-})
-
-//destroy
-app.delete('/posts/:id', (req,res) => {
-    res.send('Eliminazione del post ' + req.params.id)
-})
+app.use('/posts/', postsRouter);
 
 app.listen(port, () => {
-    console.log('Server in ascolto sulla porta ' + port)
-})
+  console.log('Server in ascolto sulla porta ' + port);
+});
---
+++ routers/posts.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const postsRouter = express.Router();
+
+// INDEX
+postsRouter.get('/', (req, res) => {
+  res.send('Lista dei posts');
+});
+
+// SHOW
+postsRouter.get('/:id', (req, res) => {
+  res.send(`Visualizzazione del post ${req.params.id}`);
+});
+
+// STORE
+postsRouter.post('/', (req, res) => {
+  res.send('Creazione nuovo post');
+});
+
+// UPDATE
+postsRouter.put('/:id', (req, res) => {
+  res.send(`Modifica del post ${req.params.id}`);
+});
+
+// DESTROY
+postsRouter.delete('/:id', (req, res) => {
+  res.send(`Eliminazione del post ${req.params.id}`);
+});
+
+module.exports = postsRouter;
